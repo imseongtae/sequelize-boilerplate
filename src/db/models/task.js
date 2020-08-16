e7df2dccbc4c7e96a1f642f2b95e217bc273ece7
imseongtae <dev.haemil@gmail.com>
2020-08-16 23:43:54 +0900

    feat: Update task model to include association

--- src/db/models/task.js
+++ src/db/models/task.js
@@ -1,23 +1,18 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
-  class Task extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
+  const Task = sequelize.define(
+    'Task',
+    {
+      taskName: DataTypes.STRING
+    },
+    {}
+  );
+  Task.associate = function(models) {
+    // associations can be defined here
+    Task.belongsTo(models.User, {
+      foreignKey: 'userId'
+    });
   };
-  Task.init({
-    taskName: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Task',
-  });
   return Task;
 };
