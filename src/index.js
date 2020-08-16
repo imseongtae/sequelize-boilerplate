7684bcac87ade612da700b65307f74f03a4809cb
imseongtae <dev.haemil@gmail.com>
2020-08-16 23:27:24 +0900

    feat: Add function to get Task API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 console.log('hello world');
+
+const db = require('./db/models')
+
+// Promises
+function getTasksPromise() {
+  db.Task.findAll()
+    .then(tasks => {
+      tasks.forEach(task => {
+        console.log(task.taskName);
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    })
+}
+
+async function getTasksAsync() {
+  try {
+    const tasks = await db.Task.findAll();
+    tasks.forEach(task => {
+      console.log(task.taskName);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// getTasksPromise();
+getTasksAsync();
